878144d1e9cac29416db157e6c83d06a5b9f62fb
Renato <renato.henrique_98@hotmail.com>
2023-09-24 10:35:18 -0300

    service example

--- src/main/java/com/github/throyer/example/controllers/HomeController.java
+++ src/main/java/com/github/throyer/example/controllers/HomeController.java
@@ -1,21 +1,19 @@
 package com.github.throyer.example.controllers;
 
+import com.github.throyer.example.services.ExampleService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
-import static java.util.Optional.ofNullable;
-
 @RestController
 @RequestMapping("/")
+@AllArgsConstructor
 public class HomeController {
+  private final ExampleService service;
+  
   @GetMapping
-  public Map<?,?> index(String name) {
-    return Map.of(
-      "message", "hello world",
-      "nome", ofNullable(name).orElse("")
-    );
+  public Object index() {
+    return service.example();
   }
 }
